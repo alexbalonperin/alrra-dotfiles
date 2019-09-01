b64ff1b37397bb343a9d1b248ee6ca35a03f48dd
Alexandre Balon-Perin <alexbp13@gmail.com>
2019-09-01 14:33:23 +0900

    add app & tools that I use

--- src/os/install/macos/misc_tools.sh
+++ src/os/install/macos/misc_tools.sh
@@ -9,6 +9,19 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 brew_install "ShellCheck" "shellcheck"
+brew_install "Tree" "tree"
+brew_install "Watch" "watch"
+brew_install "Java" "java" "caskroom/cask" "cask"
+brew_install "Scala" "scala"
+brew_install "Sbt" "sbt"
+brew_install "Neovim" "neovim"
+brew_install "Ag" "the_silver_searcher"
+brew_install "Diff so fancy" "diff-so-fancy"
+brew_install "GPG" "gpg"
+brew_install "JQ" "jq"
+brew_install "Tor" "tor"
+brew_install "Docker" "docker"
+brew_install "Docker Compose" "docker-compose"
 #brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
 
 if [ -d "$HOME/.nvm" ]; then
