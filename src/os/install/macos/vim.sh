091627085a225e5cbbb6bc36533373d7ea3016f4
Alexandre Balon-Perin <alexbp13@gmail.com>
2019-09-01 22:11:21 +0900

    fix vim install

--- src/os/install/macos/vim.sh
+++ src/os/install/macos/vim.sh
@@ -5,6 +5,25 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+install_metals() {
+  brew_tap "coursier/formulas"
+  brew_install "Coursier" "coursier/formulas/coursier"
+  execute "coursier bootstrap \
+              --java-opt -Xss4m \
+              --java-opt -Xms100m \
+              --java-opt -Dmetals.client=coc.nvim \
+              org.scalameta:metals_2.12:0.7.0 \
+              -r bintray:scalacenter/releases \
+              -r sonatype:snapshots \
+              -o /usr/local/bin/metals-vim -f" \
+    "Generate metals binary"
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 brew_install "Neovim" "neovim"
 brew_install "Python" "python"
 pip3 install pynvim
+
+install_metals
--- src/os/install/vim.sh
+++ src/os/install/vim.sh
@@ -5,13 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-copy_config() {
-  execute "ln -s ./vim/init.vim $HOME/.config/nvim/init.vim" \
-    "Copy nvim config"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 install_plugins() {
     execute "nvim --headless +PlugUpgrade +PlugInstall +UpdateRemotePlugins +qa" \
       "Install plugins"
@@ -19,35 +12,13 @@ install_plugins() {
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_metals() {
-  brew_tap "coursier/formulas"
-  brew_install "Coursier" "coursier/formulas/coursier"
-  execute "coursier bootstrap \
-              --java-opt -Xss4m \
-              --java-opt -Xms100m \
-              --java-opt -Dmetals.client=coc.nvim \
-              org.scalameta:metals_2.12:0.7.0 \
-              -r bintray:scalacenter/releases \
-              -r sonatype:snapshots \
-              -o /usr/local/bin/metals-vim -f" \
-    "Generate metals binary"
-
-  execute "cp ./vim/coc-settings.json $HOME/.config/nvim/coc-settings.json" \
-    "Copy nvim config"
-
-
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
 
     print_in_purple "\n   Vim\n\n"
 
     "./$(get_os)/vim.sh"
-    copy_config
     install_plugins
-    install_metals
 
 }
 
