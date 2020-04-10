10ba54ec9ac5efcfebc397450af1eafb855295f6
Alexandre Balon-Perin <alexbp13@gmail.com>
2020-04-10 23:34:00 +0900

    add exfat filesystem support

---
+++ src/os/install/ubuntu/filesystem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   ExFat\n\n"
+install_package "exfat-fuse exfat-utils"
