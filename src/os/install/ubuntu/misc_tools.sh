ecf477dcf6a98b40d530d554153c4b6567f24381
Alexandre Balon-Perin <alexbp13@gmail.com>
2020-04-10 23:59:10 +0900

    add hook for direnv

--- src/os/install/ubuntu/misc_tools.sh
+++ src/os/install/ubuntu/misc_tools.sh
@@ -15,6 +15,8 @@ install_package "Gnome Tweak Tool" "gnome-tweak-tool"
 install_package "Ag" "silversearcher-ag"
 install_package "HTop" "htop"
 install_package "Direnv" "direnv"
+execute "echo \"eval '\$(direnv hook bash)'\" >> ~/.bashrc" \
+  "Hook direnv"
 
 execute "curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > /tmp/diff-so-fancy && sudo mv /tmp/diff-so-fancy /usr/local/bin/diff-so-fancy && sudo chmod u+x /usr/local/bin/diff-so-fancy" \
   "Installing diff-so-fancy"
