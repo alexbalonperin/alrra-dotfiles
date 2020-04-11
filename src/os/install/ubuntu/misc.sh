3095177f09975852da37ba4aa65290cc43a9bbc8
Alexandre Balon-Perin <alexbp13@gmail.com>
2020-04-11 14:44:07 +0900

    add install lastpass

--- src/os/install/ubuntu/misc.sh
+++ src/os/install/ubuntu/misc.sh
@@ -13,7 +13,7 @@ install_package "VLC" "vlc"
 
 if ! package_is_installed "spotify-client"; then
   add_key "https://download.spotify.com/debian/pubkey.gpg" \
-        || print_error "Spotify (add key)"
+    || print_error "Spotify (add key)"
   add_to_source_list "http://repository.spotify.com stable non-free" "spotify.list" \
     || print_error "Spotify (add to package resource list)"
 
@@ -22,5 +22,21 @@ if ! package_is_installed "spotify-client"; then
 
 fi
 install_package "Spotify" "spotify-client"
+
+
+LASTPASS_FOLDER="lplinux"
+LASTPASS_ARCHIVE="$LASTPASS_FOLDER.tar.bz2"
+
+if [ ! -f "/tmp/$LASTPASS_ARCHIVE" ]; then
+  execute "curl https://download.cloud.lastpass.com/linux/$LASTPASS_ARCHIVE > /tmp/$LASTPASS_ARCHIVE" \
+    "Lastpass (Download archive)"
+fi
+
+mkd "/tmp/$LASTPASS_FOLDER"
+execute "sudo tar xjvf /tmp/$LASTPASS_ARCHIVE -C /tmp/$LASTPASS_FOLDER" \
+  "Lastpass (Extract archive)"
+
+execute "cd /tmp/$LASTPASS_FOLDER && ./install_lastpass.sh" \
+  "Lastpass (Install)"
 
 printf "\n"
