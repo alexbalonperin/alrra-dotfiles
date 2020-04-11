83d4c7e28fe87a941e67d6a594c5e514eeade3c6
Alexandre Balon-Perin <alexbp13@gmail.com>
2020-04-11 14:18:41 +0900

    add install spotify

--- src/os/install/ubuntu/misc.sh
+++ src/os/install/ubuntu/misc.sh
@@ -10,3 +10,17 @@ print_in_purple "\n   Miscellaneous\n\n"
 
 install_package "Transmission" "transmission"
 install_package "VLC" "vlc"
+
+if ! package_is_installed "spotify-client"; then
+  add_key "https://download.spotify.com/debian/pubkey.gpg" \
+        || print_error "Spotify (add key)"
+  add_to_source_list "http://repository.spotify.com stable non-free" "spotify.list" \
+    || print_error "Spotify (add to package resource list)"
+
+  update &> /dev/null \
+    || print_error "Spotify (resync package index files)"
+
+fi
+install_package "Spotify" "spotify-client"
+
+printf "\n"
